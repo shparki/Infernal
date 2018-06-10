55e0478691191c4cfd2853ceb050bcea8e749e41
Matt "Shparki" Murray <murrmat@gmail.com>
2018-06-09 22:09:17 -0500

    Beginning a new era for 'Session'

--- infernal/core/session.py
+++ infernal/core/session.py
@@ -5,137 +5,86 @@ from .utils import default_dict
 import requests
 import datetime
 import os
-import logging
+# import logging
 import csv
+import json
 
 import pandas as pd
 import queue
-import threading
 import time
 
 
-class Session(object):
 
-	def __init__ (self, api_key, endpoint='na-old', name=''):
-		
-		# Save api_key
+class RiotAuth(requests.auth.AuthBase):
+	''' Basic authenticator, extending the Requests AuthBase to be used with 
+	riot header requests. Simply adds the 'X-Riot_Token' headers key with
+	the definied api_key to the riot api call
+	'''
+	def __init__(self, api_key):
 		self.api_key = api_key
 
-		# Check if the endpoint is valid; if not default to 'na-old'
+	def __call__(self, r):
+		r.headers['X-Riot-Token'] = self.api_key
+
+class Session(object):
+
+	def __init__(self, api_key, endpoint='na-old', name=None):
+		
+		# save the api_key associated with this session; each session should 
+		# have their own api key
+		self.api_key = api_key
+
+		# build the default authenticator for the session:
+		self.auth = RiotAuth(self.api_key)
+
+		# Check if the endpoint is valid; if so then use, otherwise use the
+		# default region
 		self.endpoint = 'na-old'
 		if endpoint in const.ENDPOINTS.keys():
 			self.endpoint = endpoint
-			self._log('Endpoint {} not found; using default endpoing \'na-old\''.format(endpoint))
 
-		# uid functions as a unique identifier for each session and the names for logging files
+		# uid (unique-id) functions as a unique identifier for each session
+		# based off the time and date of the created session
 		self.uid = datetime.datetime.today().strftime('%y%m%d_%H%M%S')
 
-		# Create data & logs folders if they do not exist- created at the location of the script
-		self.data_dpath = os.getcwd() + '/data'
-		self.logs_dpath = os.getcwd() + '/logs'
+		# set the name for the session; if unnamed, the session name
+		# will default to the uid
+		self.name = name
+		if self.name is None:
+			self.name = self.uid
 
-		if not os.path.exists(self.data_dpath):
-			os.mkdir(self.data_dpath)
+		# set the location for the data/logs file paths; will default to 
+		# /{name}/data & /{name}/logs. cwd is current working directory.
+		# if directories don't exist, then create them
+		cwd = str(os.getcwd())
+		self.parent_dirpath = '{}/{}'.format(cwd, self.name)
+		if not os.path.exists(self.parent_dirpath):
+			os.mkdir(self.parent_dirpath)
 
-		if not os.path.exists(self.logs_dpath):
-			os.mkdir(self.logs_dpath)
+		self.data_dirpath = '{}/data'.format(self.parent_dirpath, self.name)
+		if not os.path.exists(self.data_dirpath):
+			os.mkdir(self.data_dirpath)
 
-		# Instantiate logger file
-		self.log_file = None
-		self.log_path = self.logs_dpath + '/' + self.uid + '.log'
-		if not os.path.exists(self.log_path):
-			self.log_file = open(self.log_path, 'w')
-			self.log_file.close()
+		self.logs_dirpath = '{}/logs'.format(self.parent_dirpath, self.name)
+		if not os.path.exists(self.logs_dirpath):
+			os.mkdir(self.logs_dirpath)
 
-		# Disable requests logging
-		urllib3_log = logging.getLogger('urllib3')
-		urllib3_log.setLevel(logging.CRITICAL)
-
-		# Initialize logging & format
-		logging.basicConfig(
-			level = 	logging.DEBUG,
-			filename = 	self.log_path,
-			format = 	'[%(levelname)s] %(asctime)s : %(message)s',
-			datefmt = 	'%I:%M:%S %p'
-		)
-
-		self._log('Initialized ' + str(self))
+		# Add logging here...
 
 		# Rate limiting logic here
 		self.request_rate = 0.5
-		self.request_throttle = 1.1
-
+		self.request_throttle = 0.8
 
 	def __str__(self):
-		return 'session_{}'.format(self.uid)
+		return 'session_{} | uid: {}'.format(self.name, self.uid)
 
 
-	
-	def build_url (self, url, url_params = {}):
+	def request(self, url, url_params, params={}):
 		args = {
-			'endpoint':	const.ENDPOINTS[self.endpoint],
-			'url':		url
+			'endpoint': const.ENDPOINTS[self.region],
+			'url': url
 		}
-		req_url = const.URLS_BASE['base'].format_map(
-			default_dict(**args))
-		req_url = req_url.format_map(
-			default_dict(**url_params))
-
-
-		return req_url
-
-	def _request(self, url, params = {}):
-		param_string = ''
-		for key, value in params.items():
-			param_string += (str(key) + '=' + str(value) + '&')
-		param_string += ('api_key=' + self.api_key)
-		url = url.format(params = param_string)
-
-		self._log('Requesting...')
-		self._log('URL: ' + str(url))
-
-		req = requests.get(url)
-
-		self._log('RESPONSE CODE: ' + str(req.status_code))
-		self._log('RESPONSE: ' + str(req.json()))
-		self._log('Done!')
-
-		return req
-
-	def request(self, url, params={}):
-		time.sleep(self.request_rate * (1 + self.request_throttle))
-
-		req = self._request(url=url, params=params)
-
-		if req.status_code >= 400:
-			raise RequestError(str(req.status_code))
-
-		return req.json()
 
 
 
-
-	def _log(self, message, level='debug', full=False):
-		if not full:
-			if len(message) > 250:
-				message = message[:150] + '...'
-
-		if level == 'info':
-			logging.info(message)
-		elif level == 'debug':
-			logging.debug(message)
-		elif level == 'warning':
-			logging.warning(message)
-		elif level == 'error':
-			logging.error(message)
-		elif level == 'critical':
-			logging.critical(message)
-		else:
-		 	logging.error('level \'%s\' does not exist. \n\t Message: %s', str(level), str(message[:250]))
-
-	def _cache(self, data):
-		pass
-
-	def _decache(self, name):
-		pass
 
