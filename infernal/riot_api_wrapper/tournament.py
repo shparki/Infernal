3ea225a4ce006b4586750bce719084d2312bc04e
Shparki <murrmat@gmail.com>
2019-12-12 00:06:45 -0600

    smal changes

--- infernal/riot_api_wrapper/tournament.py
+++ infernal/riot_api_wrapper/tournament.py
@@ -2,124 +2,124 @@ from ..core import Session
 from ..core import constants as const
 
 class Tournament(object):
-	version = const.VERSIONS['tournament']
+    version = const.VERSIONS['tournament']
 
-	@classmethod
-	def getCodes(cls, session):
-		session._log('Calling getCodes...')
-		url = session.build_url(
-			url = const.URLS_TOURN['clodes'],
-			url_params = {
-				'version':			cls.version
-			}
-		)
+    @classmethod
+    def getCodes(cls, session):
+        session._log('Calling getCodes...')
+        url = session.build_url(
+            url = const.URLS_TOURN['clodes'],
+            url_params = {
+                'version':			cls.version
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getTournamentDTO(cls, session, tournament_code):
-		session._log('Calling getTournamentDTO...')
-		url = session.build_url(
-			url = const.URLS_TOURN['by tournament'],
-			url_params = {
-				'version':			cls.version,
-				'tournament_code':	str(tournament_code)
-			}
-		)
+    @classmethod
+    def getTournamentDTO(cls, session, tournament_code):
+        session._log('Calling getTournamentDTO...')
+        url = session.build_url(
+            url = const.URLS_TOURN['by tournament'],
+            url_params = {
+                'version':			cls.version,
+                'tournament_code':	str(tournament_code)
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getEvents(cls, session, tournament_code):
-		session._log('Calling getEvents...')
-		url = session.build_url(
-			url = const.URLS_TOURN['events by tournament'],
-			url_params = {
-				'version':			cls.version,
-				'tournament_code':	str(tournament_code)
-			}
-		)
+    @classmethod
+    def getEvents(cls, session, tournament_code):
+        session._log('Calling getEvents...')
+        url = session.build_url(
+            url = const.URLS_TOURN['events by tournament'],
+            url_params = {
+                'version':			cls.version,
+                'tournament_code':	str(tournament_code)
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getProviders(cls, session):
-		session._log('Calling getProviders...')
-		url = session.build_url(
-			url = const.URLS_TOURN['providers'],
-			url_params = {
-				'version':			cls.version
-			}
-		)
+    @classmethod
+    def getProviders(cls, session):
+        session._log('Calling getProviders...')
+        url = session.build_url(
+            url = const.URLS_TOURN['providers'],
+            url_params = {
+                'version':			cls.version
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getTournaments(cls, session):
-		session._log('getTournaments...')
-		url = session.build_url(
-			url = const.URLS_TOURN('tournaments'),
-			url_params = {
-				'version': cls.version
-			}
-		)
+    @classmethod
+    def getTournaments(cls, session):
+        session._log('getTournaments...')
+        url = session.build_url(
+            url = const.URLS_TOURN('tournaments'),
+            url_params = {
+                'version': cls.version
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
-			
-		return r
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
+            
+        return r
 
 
 
--- infernal/riot_api_wrapper/tournament_stub.py
+++ infernal/riot_api_wrapper/tournament_stub.py
@@ -2,99 +2,99 @@ from ..core import Session
 from ..core import constants as const
 
 class TournamentStub(object):
-	version = const.VERSIONS['tstub']
+    version = const.VERSIONS['tstub']
 
-	@classmethod
-	def getCode(cls, session):
-		session._log('Calling getCode...')
-		url = session.build_url(
-			url = const.URLS_TSTUB['codes'],
-			url_params = {
-				'version':			cls.version
-			}
-		)
+    @classmethod
+    def getCode(cls, session):
+        session._log('Calling getCode...')
+        url = session.build_url(
+            url = const.URLS_TSTUB['codes'],
+            url_params = {
+                'version':			cls.version
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getCodesByTournament(cls, session, tournament_code):
-		session._log('Calling getCodesByTourn...')
-		url = session.build_url(
-			url = const.URLS_TSTUB['by tournament'],
-			url_params = {
-				'version':			cls.version,
-				'tournament_code':	str(tournament_code)
-			}
-		)
+    @classmethod
+    def getCodesByTournament(cls, session, tournament_code):
+        session._log('Calling getCodesByTourn...')
+        url = session.build_url(
+            url = const.URLS_TSTUB['by tournament'],
+            url_params = {
+                'version':			cls.version,
+                'tournament_code':	str(tournament_code)
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getProviders(cls, session, tournament_code):
-		session._log('Calling getProviders...')
-		url = session.build_url(
-			url = const.URLS_TSTUB['providers'],
-			url_params = {
-				'version':			cls.version
-			}
-		)
+    @classmethod
+    def getProviders(cls, session, tournament_code):
+        session._log('Calling getProviders...')
+        url = session.build_url(
+            url = const.URLS_TSTUB['providers'],
+            url_params = {
+                'version':			cls.version
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-	@classmethod
-	def getTournaments(cls, session):
-		session._log('Calling getTournaments...')
-		url = session.build_url(
-			url = const.URLS_TSTUB['tournaments'],
-			url_params = {
-				'version':			cls.version
-			}
-		)
+    @classmethod
+    def getTournaments(cls, session):
+        session._log('Calling getTournaments...')
+        url = session.build_url(
+            url = const.URLS_TSTUB['tournaments'],
+            url_params = {
+                'version':			cls.version
+            }
+        )
 
-		try:
-			r = session._request(url, params=params)
-		except RequestError as req_err:
-			print(req_err)
-			session._log(req_err, level='error')
-			return pd.nan
-		except Exception as e:
-			print(e)
-			session._log(e, level='error')
-			return pd.nan
+        try:
+            r = session._request(url, params=params)
+        except RequestError as req_err:
+            print(req_err)
+            session._log(req_err, level='error')
+            return pd.nan
+        except Exception as e:
+            print(e)
+            session._log(e, level='error')
+            return pd.nan
 
-		return r
+        return r
 
-		
+        
